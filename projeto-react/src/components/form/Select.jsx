dcc37cd90661a6badf0c0314abff5d6246c37107
MarianaCoradiniOliveira <mariana.coradinioliveira@gmail.com>
2024-03-07 20:50:31 -0300

    continuacao do projeto - mas commit com erro, arrumar no proximo dia

--- projeto-react/src/components/form/Select.jsx
+++ projeto-react/src/components/form/Select.jsx
@@ -1,11 +1,14 @@
 import styles from './Select.module.css'
 
-function Select({text, name, options, handleOnChance, value}){
+function Select({text, name, options, handleOnChange, value}){
     return(
         <div className={styles.form_control}>
         {/* Adiciona um rótulo associado ao input. O atributo htmlFor está configurado com o valor de name, associando o rótulo ao input correspondente. */}
         <label htmlFor={name}>{text}:</label> 
-        <select name={name} id={name}>
+        {/* name e id: Identificadores para o elemento de seleção.
+        onChange: É a função que será chamada quando o valor selecionado for alterado. Geralmente, handleOnChange é uma função definida em algum lugar no seu código que lida com a mudança do elemento de seleção.
+        value: Define o valor atualmente selecionado no elemento de seleção. Este valor é controlado pelo estado do componente (provavelmente value). */}
+        <select name={name} id={name} OnChange={handleOnChange} value={value || ''}>
             <option>Selecione uma opção</option>
             {options.map((option) => (
                 <option value={option.id} key={option.id}>{option.name}</option> //Para cada item (option) na lista, estamos criando um elemento de opção. Cada opção tem um valor (value) definido como o id do item e um texto visível ({option.name}). O key é uma propriedade especial para ajudar o React a rastrear e gerenciar esses elementos de forma eficiente.
--- projeto-react/src/components/project/ProjectForm.jsx
+++ projeto-react/src/components/project/ProjectForm.jsx
@@ -1,24 +1,23 @@
 import { useEffect, useState } from 'react'
 
-import styles from './Project.module.css'
+import styles from './ProjectFrom.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
-function ProjectForm({btnText}){
+function ProjectForm({handleSubmit, btnText, projectData}){
 
     const [categories, setCategories] = useState([]) //Este trecho utiliza o hook useState para criar um estado chamado categories e uma função setCategories para atualizar esse estado. O estado é inicializado como um array vazio ([]).
-
-
+    const [project, setProject] = useState(projectData || {})
 
     //Um "hook" em React é uma função especial que permite que você use o estado e outros recursos do React em componentes funcionais. Em outras palavras, hooks são ferramentas que tornam mais fácil escrever componentes em React sem a necessidade de usar classes.
 
     useEffect(() => { //Neste código, o useEffect está sendo usado para buscar e carregar dados de categorias assim que o componente é montado.
-        fetch("http://localhost:5000/categories", { //Estamos pedindo ao computador para buscar dados em uma certa página web (um endereço local http://localhost:5000/categories).
+        fetch('http://localhost:5000/categories', { //Estamos pedindo ao computador para buscar dados em uma certa página web (um endereço local http://localhost:5000/categories).
         method: "GET", 
         headers: {
-            'Content-Type': 'application/json'
-        }
+            'Content-Type': 'application/json',
+        },
     })
         .then((resp) => resp.json()) //Quando os dados forem trazidos, esta parte diz ao computador para entender que esses dados estão em formato de texto (JSON) e transformá-los em algo que possamos usar no nosso programa.
         .then((data) =>{ // Aqui, estamos pegando esses dados transformados (agora em um formato que podemos entender) e guardando-os no espaço categories que criamos anteriormente.
@@ -27,11 +26,31 @@ function ProjectForm({btnText}){
         .catch((err) => console.log(err)) //Se alguma coisa der errado durante esse processo, como se o site não responder, esta parte nos ajuda a descobrir o que deu errado ao mostrar uma mensagem de erro no console
     }, [])
 
+    const submit = (e) =>{ //Aqui, está sendo declarada uma função chamada submit. Essa função recebe um argumento e, que é um evento associado a uma ação (como um clique em um botão de envio de formulário).
+        e.preventDefault() //No contexto de um formulário, isso significa que a página não será recarregada quando o formulário for enviado. 
+        console.log(project)
+        //handleSubmit(project)
+    }
+
+    function handleChange(e){
+        setProject({...project, [e.target.name]: e.target.value}) //é utilizada para atualizar dinamicamente o estado project conforme os campos de um formulário são alterados. Cada vez que um campo de formulário muda, essa função é chamada, atualizando o estado do componente com os valores mais recentes do formulário
+        console.log(project)
+    }
+
+    function handleCategory(e){ //é utilizada para atualizar dinamicamente o estado do componente (project), especialmente a propriedade category, conforme a seleção de uma opção em um elemento de seleção. Cada vez que uma opção é selecionada, essa função é chamada, atualizando o estado do componente com os valores mais recentes da opção selecionada.
+        setProject({
+            ...project, category: {
+                id: e.target.value,
+                name: e.target.options[e.target.selectedIndex].text,
+            },
+        })
+    }
+
     return(
-        <form className={styles.form}>
-            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto"/>
-            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total"/>
-            <Select name="category_id" text="Selecione a categoria" options={categories}/>
+        <form onSubmit={submit} className={styles.form}>
+            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" handleOnChange={handleChange}/>
+            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total" handleOnChange={handleChange}/>
+            <Select name="category_id" text="Selecione a categoria" handleOnChange={handleCategory} options={categories}/>
             <SubmitButton text={btnText}/>
         </form>
     )
